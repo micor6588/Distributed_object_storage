226d7a1aa759f9a92156cec1a670ca72992a0401
micor6588 <55865558+micor6588@users.noreply.github.com>
2021-05-13 21:25:33 +0800

    封装rabbitMQ函数库的rabbitmq包

---
+++ Distributed_storage_system/rabbitmq/rabbitmq.go
@@ -0,0 +1,93 @@
+// 对github.com/streadway/amqp"包进行封装，用于简化接口
+
+package rabbitmq
+
+import (
+	"encoding/json"
+
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQ struct {
+	channel  *amqp.Channel
+	Name     string
+	exchange string
+}
+
+func New(s string) *RabbitMQ {
+	conn, e := amqp.Dial(s)
+	if e != nil {
+		panic(e)
+	}
+
+	ch, e := conn.Channel()
+	if e != nil {
+		panic(e)
+	}
+
+	q, e := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		true,  //delete when unused
+		false, // exclusive
+		false, //n o-wait
+		nil,   // arguments
+	)
+	if e != nil {
+		panic(e)
+	}
+
+	mq := new(RabbitMQ)
+	mq.channel = ch
+	mq.Name = q.Name
+	return mq
+}
+
+func (q *RabbitMQ) Bind(exchage string) {
+	e := q.channel.QueueBind(
+		q.Name,
+		"",
+		exchage,
+		false,
+		nil,
+	)
+	if e != nil {
+		panic(e)
+	}
+	q.exchange = exchage
+}
+
+func (q *RabbitMQ) Send(queue string, body interface{}) {
+	str, e := json.Marshal(body)
+	if e != nil {
+		panic(e)
+	}
+	e = q.channel.Publish("",
+		queue, false, false,
+		amqp.Publishing{
+			ReplyTo: q.Name,
+			Body:    []byte(str),
+		})
+	if e != nil {
+		panic(e)
+	}
+}
+
+func (q *RabbitMQ) Consume() <-chan amqp.Delivery {
+	c, e := q.channel.Consume(q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if e != nil {
+		panic(e)
+	}
+	return c
+}
+
+func (q *RabbitMQ) Close() {
+	q.channel.Close()
+}
